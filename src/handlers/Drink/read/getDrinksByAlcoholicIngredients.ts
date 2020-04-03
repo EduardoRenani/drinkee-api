b006aca68a75ae67fab5007d030f499e6bc39c18
Eduardo Renani <eduardo.renani@hotmail.com>
2020-04-03 12:36:48 -0300

    Get Drink by Alcoholic Ingredients with optional params working

--- src/handlers/Drink/read/getDrinksByAlcoholicIngredients.ts
+++ src/handlers/Drink/read/getDrinksByAlcoholicIngredients.ts
@@ -3,14 +3,28 @@ import { DrinkService } from '../../../services/DrinkService';
 import {  buildResponseError } from '../../../../lib/drinkeeAPIGatewayProxyResult';
 import { APIGatewayProxyHandler } from "aws-lambda";
 import * as Logger from '../../../../lib/logger';
+import { isNotEmpty } from 'class-validator';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
     Logger.info(event);
     try {
-        const baseSpirit = event.queryStringParameters.baseSpirit.replace("%20", " ");
-        const liquor = event.queryStringParameters.liquor.replace("%20", " ");
-        const wineVermouth = event.queryStringParameters.wineVermouth.replace("%20", " ");
-        const mixers = event.queryStringParameters.mixers.replace("%20", " ");
+
+        const baseSpirit = 
+            isNotEmpty(event.queryStringParameters.baseSpirit) ? 
+            event.queryStringParameters.baseSpirit.replace("%20", " ") : 
+            null
+        const liquor = 
+            isNotEmpty(event.queryStringParameters.liquor) ?
+            event.queryStringParameters.liquor.replace("%20", " ") :
+            null
+        const wineVermouth = 
+            isNotEmpty(event.queryStringParameters.wineVermouth) ?
+            event.queryStringParameters.wineVermouth.replace("%20", " ") :
+            null
+        const mixers =
+            isNotEmpty(event.queryStringParameters.mixers) ?
+            event.queryStringParameters.mixers.replace("%20", " ") :
+            null
 
         return success(await new DrinkService()
             .getDrinksByAlcoholicIngredients(baseSpirit, liquor, wineVermouth, mixers)
